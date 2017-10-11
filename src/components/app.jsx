5878161a74838f64b422f41ccddde8545889e208
roelver <roel@romaniflo.nl>
2017-10-11 21:06:03 +0200

    lint fixes

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
-  constructor(props) {
+
+  constructor (props) {
     super(props);
     this.state = { test: true };
   }
-  render() {
+
+  render () {
     return (
       <h1 className="app-root">Hello from React</h1>
     );
